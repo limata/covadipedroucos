a04ddd287f58e008e79eea11283ed2eb095395ab
antonio lima <a.m.maiadelima@gmail.com>
2018-01-25 00:05:19 +0000

    re-start project already with user model

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'contact', to: 'pages#contact'
+  get 'about', to: 'pages#about'
+
+  root to: 'pages#home'
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
